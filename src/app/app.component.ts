b4ea4a00881b328310c03a39a02a1d7a5183f77f
Turbay <youtopinc@gmail.com>
2019-01-18 12:58:03 -0400

    first commit1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Component } from '@angular/core';
 
 
 export class AppComponent {
-  title = 'CentroMonitoreo12';
+  title = 'CentroMonitoreo';
 
   functionOnWhichRedirectShouldHappen()
   { 
